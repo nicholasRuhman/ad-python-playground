77570385e35cd13dc2acec0289a759748503d225
byem <matthew.bye@autodesk.com>
2021-02-11 17:27:27 -0800

    remove obj2 working implementation

--- objective_2/objective_2.py
+++ objective_2/objective_2.py
@@ -1,2 +1,7 @@
-def chop(chars,word):
-    return (word[:chars],word[chars:])
+# Implement the following function:
+#
+# Function Name: chop
+# Input type: <int>, <string>
+# Return type: <tuple>
+#
+# Description: Returns a tuple consisting of the leftmost <int> characters and rightmost <int> characters of the <string>
